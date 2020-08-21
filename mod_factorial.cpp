93707495f30707aa1ecc3433861a6089fdfedd89
mitoh0311 <kirosingo0311@gmail.com>
2020-08-21 21:34:15 +0900

    mod libs

--- mod_factorial.cpp
+++ mod_factorial.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-template<typename T> T mod_factorial(T in, T mod) {
+template<typename T> T mod_factorial(T in, T mod = 1000000007) {
     long long ans = 1;
     for (int i=2; i < in + 1; ++i) {
         ans *= i;

b78912e47a920ba1423caf8b9a4ca0176aa0a281
mitoh0311 <kirosingo0311@gmail.com>
2020-09-09 21:17:27 +0900

    added template file

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+#define ALL(x) (x).begin(), (x).end()
+#define pb push_back
+#define mod 1000000007
+#define mod9 998244353
+
+using namespace std;
+using ll = long long;
+
+template <typename T> bool chmax(T &a, const T& b) {if(a < b){a=b; return true;} return false;}
+template <typename T> bool chmin(T &a, const T& b) {if(a > b){a=b; return true;} return false;}
+
+
+int main(){
+    cout << "" << endl;
+}

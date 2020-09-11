8d3df5ca5a9fad2b739752ce9de575f374b1302e
itoh <44693685+mitoh0311@users.noreply.github.com>
2020-09-11 16:32:30 +0900

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -3,11 +3,11 @@
 
 #define ALL(x) (x).begin(), (x).end()
 #define pb push_back
-#define mod 1000000007
-#define mod9 998244353
+const int MOD = 1000000007;
+const int MOD9 = 998244353;
 
 using namespace std;
-using ll = long long;
+typedef long long ll;
 
 template <typename T> bool chmax(T &a, const T& b) {if(a < b){a=b; return true;} return false;}
 template <typename T> bool chmin(T &a, const T& b) {if(a > b){a=b; return true;} return false;}

0421427c2a9b0b88f14a6d3e559ab9a95bea6526
mitoh0311 <kirosingo0311@gmail.com>
2021-01-05 06:56:01 +0900

    update

--- python/ncr.py
+++ python/ncr.py
@@ -1,12 +1,27 @@
-from math import factorial
+MOD = 10 ** 9 + 7
+MAX = 200000
+fac = [0] * MAX
+finv = [0] * MAX
+inv = [0] * MAX
 
-def nCr(n, r, mod):
-    a = 1
-    for i in range(r):
-        a *= (n - i)
-        a %= mod
-    return (a * pow(factorial(r), mod-2, mod)) % mod
+def combinit():
+    fac[0] = 1
+    fac[1] = 1
+    finv[0] = 1
+    finv[1] = 1
+    inv[1] = 1
+    for i in range(2, MAX):
+        fac[i] = fac[i - 1] * i % MOD
+        inv[i] = MOD - inv[MOD % i] * (MOD // i) % MOD
+        finv[i] = finv[i - 1] * inv[i] % MOD
+    
+def comb(n, k):
+    if n < k: return 0
+    if (n < 0 or k < 0): return 0
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD
 
-# print(nCr(5, 2, 10 ** 9 + 7))
-#
-# 10
+"""
+combinit()
+print(comb(5, 2))
+print(comb(1000, 500))
+"""
